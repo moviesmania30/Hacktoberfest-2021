e18959f25b51d37e3da10e57e9f7be2630b7f135
anupmaharzn <hellen7203@gmail.com>
2021-10-19 15:04:40 +0545

    added program to check palindrome number in c

---
+++ C Language/ProgramToCheckPalindrome-number.c
@@ -0,0 +1,31 @@
+//An integer is a palindrome if the reverse of that number is equal to the original number
+
+//Program to check palindrome
+
+#include <stdio.h>
+int main() {
+  int n, reversed = 0, remainder, original;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    original = n;
+
+    // reversed integer is stored in reversed variable
+    while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+    // palindrome if orignal and reversed are equal
+    if (original == reversed)
+        printf("%d is a palindrome.", original);
+    else
+        printf("%d is not a palindrome.", original);
+
+    return 0;
+}
+
+
+//output sample
+//Enter an integer:1001
+//1001 is a palindrome.
